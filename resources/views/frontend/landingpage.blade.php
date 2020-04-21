a9168ae222b7dfcb011a270767f84f466d6b83ce
AndreasRobrahn <andreasrobrahn@Andreass-MacBook-Pro.local>
2020-04-21 22:57:09 +0200

    designupdate

--- resources/views/frontend/landingpage.blade.php
+++ resources/views/frontend/landingpage.blade.php
@@ -8,10 +8,10 @@
     <h5><a class="nav-link text-white font-weight-bolder" href="{{route('frontend.curriculum')}}">Lebenslauf</a></h5>
   </li>
   <li class="nav-item">
-    <h5><a class="nav-link text-white" href="#">Weisheiten des Andreas Robrahn</a></h5>
+    <h5><a class="nav-link text-white font-weight-bolder" href="#">Weisheiten des Andreas Robrahn</a></h5>
   </li>
   <li class="nav-item">
-    <h5><a class="nav-link text-white" href="{{route('frontend.examples')}}">Progammierbeispiele</a></h5>
+    <h5><a class="nav-link text-white font-weight-bolder" href="{{route('frontend.examples')}}">Progammierbeispiele</a></h5>
   </li>
 
 </ul>
@@ -21,7 +21,7 @@
       <img src="{{ asset('images/ARobrahn.jpg')}}" style="max-width: 500px">
     </div>
     <div class="col">
-      <p class="text-justify text-primary text-wrap font-weight-bold"> Sehr geehrter Besucher. Vermutlich bist du aufgrund meiner Bewerbung hier. Auf dieser Seite kannst du dir einen besserer Uberblick uber meine Faigkeiten und Arbeitshistorie machen. Viel Spaß, Andreas Robrahn</p>
+      <p class="text-justify text-info text-wrap font-weight-bold"> Sehr geehrter Besucher. Vermutlich bist du aufgrund meiner Bewerbung hier. Auf dieser Seite kannst du dir einen besserer Überblick uber meine Fähigkeiten und Arbeitshistorie machen. Unter dem Reiter "Lebenslauf" findest du einen <b> erweiterten Lebenslauf</b> und eine Seite in der mehr Zeit ins Desing geflossen ist. Der selbstironische Reiter "Weisheiten des.." befindet sich noch in der Bearbeitung. Unter "Programmierbeispiele" schalte ich nach und nach einen kleinen Katalog meiner bisher erworbenen Kenntnisse frei. Viel Spaß, Andreas Robrahn</p>
   </div>
 </div>
 
